be9d5c07440bc9572fdffee3729e78790d6a6e77
anandmatkar <anandmatkar123#>
2024-05-30 12:23:34 +0530

    added response file

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,4 +1,12 @@
+const { catchRes, successRes, SwrRes } = require("../utils/response")
+
 module.exports.createUser = async (req, res, next) => {
     let { firstName, lastName, email, password, mobile } = req.body
+
+    try {
+
+    } catch (error) {
+        return catchRes(res, error)
+    }
 
 }
---
+++ src/utils/response.js
@@ -0,0 +1,28 @@
+const successRes = (res, status, success, message, data) => {
+    return res.status(status).json({
+        success: success,
+        status: status,
+        message: message,
+        data: data ? data : null
+    })
+}
+
+const SwrRes = (res) => {
+    return res.status(400).json({
+        success: false,
+        message: 'Something Went Wrong'
+    })
+}
+
+const catchRes = (res, error) => {
+    return res.status(500).json({
+        success: false,
+        message: error.message
+    })
+}
+
+module.exports = {
+    successRes,
+    SwrRes,
+    catchRes
+}
