f6a1d1933f1b26d88d2e6244eaccf29416dd5079
Chetangit123 <pchetan839@gmail.com>
2024-08-08 12:32:05 +0530

    changes in limit

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const { winstonLogMiddleware } = require("./src/middlewares/loggerMiddleware");
 let PORT = process.env.APP_PORT || 3007;
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000,
-  max: 100,
+  max: 500,
   message: "Too many requests from this IP, please try again after 15 minutes",
 });
 app.use(limiter);
